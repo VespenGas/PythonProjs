fccbd605319d4e1051e0ebb19aeadf22a1e8220e
Evgeny Manturov <zheka.ya2000@gmail.com>
2023-11-03 19:19:07 +0200

    added Australia to proj

--- ProjDS/main.py
+++ ProjDS/main.py
@@ -2,7 +2,14 @@
 # -*- coding: utf-8 -*-
 #%%
 import pandas as pd
+import geopandas as gpd
 import os
+import seaborn as sns
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import folium
+from folium.plugins import HeatMap
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score, calinski_harabasz_score, davies_bouldin_score
 
@@ -35,18 +42,56 @@ with open('stations-georss.php', 'r') as file:
     php_contents = file.read()
 
 #Process column structure
+
+use_nsw_only = True
+
 fs_df = parse_fs_php(php_contents, ['title', 'geo:lat', 'geo:long'])
 fs_df[['geo:lat', 'geo:long']] = fs_df[['geo:lat', 'geo:long']].astype(float)
 client_locs[['LON', 'LAT']] = client_locs[['LON', 'LAT']].astype(float)
 fs_df[['geo:lat', 'geo:long']].round(6)
 fs_df['title'] = fs_df['title'].str.rstrip(' Fire Station')
 client_locs.columns = [x.lower() for x in client_locs.columns]
-fs_df.rename(columns={'geo:lat' : 'lat', 'geo:long' : 'long'}, inplace=True)
+fs_df.rename(columns={'geo:lat' : 'lat', 'geo:long' : 'lon'}, inplace=True)
+
+if use_nsw_only==True:
+    client_locs = client_locs[client_locs['region']=='NSW']
+    
 
 print('Dataframe example data:\n')
+print(f'>There are {client_locs.shape[0]} clients in the area.\n')
 print('Clients:\n')
 print(client_locs.head(10))
+print(f'>There are {fs_df.shape[0]} services in the area.\n')
 print('\nServices:\n')
 print(fs_df.head(10))
+#%%
+#Read GPD shapefile
+print("\nExtracting shapefile (Australia)...\n")
+assert os.path.isdir("AU_SPF"), 'No Australia shapefile found'
+
+australia = gpd.read_file('AU_SPF/STE_2021_AUST_GDA2020.shp').set_index('STE_CODE21')
+if use_nsw_only==True:
+    australia = australia[australia['STE_NAME21']=='New South Wales']
+australia = australia['geometry']
+
+#print(australia.crs)
+#print(australia.columns)
+
+#%%
+#Print Australia image + fire stations
+print('\nPlotting...')
+scale=6
+_size=1
+
+_figsize = np.asarray(matplotlib.rcParams['figure.figsize'])
+sns.set_style("whitegrid")
+fig, ax = plt.subplots(figsize=_figsize*scale)
+fs_plot = sns.scatterplot(ax = ax, data=fs_df, x='lon', y='lat', legend=False, zorder=3, s=scale**(1.5)*_size*10)
+
+australia.plot(ax = ax, color='lightgrey', edgecolor='grey', linewidth=scale/3, zorder=1)
+
+#sns.scatterplot(ax = ax, data=client_locs, x='lon', y='lat', zorder=2, s=scale**(1.5)*_size*2, color='red')
+
+
 
 
